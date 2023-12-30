0ac97cc6a7769ecf55aeca501aa9c99cfddf844f
Nikkolathi <mainginicholus@gmail.com>
2023-12-30 03:27:51 +0000

    First flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     """ Prints a Hello HBNB  Message when / is called """
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
     """['main'] Function """
-    app.run(host='0.0.0.0',port = 5000)
+    app.run(host='0.0.0.0',port = '5000')
